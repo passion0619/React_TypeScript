ff340384e161a6d2982f543b7d547cac9962db42
Kevin Edwards <kevin.bigdream@gmail.com>
2024-01-16 04:22:18 -0800

    HomePage Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 import "./App.css";
 import ProjectPage from "./projects/ProjectsPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import HomePage from "./home/HomePage";
 
 function App() {
   return (
-    <div className="container">
-      <ProjectPage />
-    </div>
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width={49} height={49} />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span> Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/home/HomePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomePage(){
+    return <h2>HOME</h2>
+}
+
+export default HomePage;
